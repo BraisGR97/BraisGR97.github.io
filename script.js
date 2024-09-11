f016095a1bf057edbcb02a2dc5a4521bb5fff013
BraisGR97 <braisgr97@gmail.com>
2024-09-11 16:54:28 +0200

    update

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const playButton = document.querySelector('.play-button');
 const phrases = {
     '1': ['Frase 11A', 'Frase 1B', 'Frase 1C', 'Frase 1D'],
     '2': ['Frase 2A', 'Frase 2B', 'Frase 2C', 'Frase 2D'],
-    '3': ['Frase 3A', 'Frase 3B', 'Frase 3C', 'Frase 3D'],
+    '3': ['Slap', 'Kiss', 'Shot', 'Selfie'],
     '4': ['Frase 4A', 'Frase 4B', 'Frase 4C', 'Frase 4D'],
     '5': ['Frase 5A', 'Frase 5B', 'Frase 5C', 'Frase 5D']
 };
